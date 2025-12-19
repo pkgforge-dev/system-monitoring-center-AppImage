9cb422a13169ad577e35c8bf49b821e85fa0ffa5
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-12-19 15:36:44 -0400

    package app

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,18 +3,26 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q system-monitoring-center | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/scalable/apps/io.github.hakandundar34coding.system-monitoring-center.svg
+export DESKTOP=/usr/share/applications/io.github.hakandundar34coding.system-monitoring-center.desktop # longest icon and desktop names ever
+export DEPLOY_OPENGL=1
+export DEPLOY_VULKAN=1
+export DEPLOY_SYS_PYTHON=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun \
+	/usr/bin/system-monitoring-center \
+	/usr/bin/usr/bin/amdgpu_top
 
-# Additional changes can be done in between here
+sed -i \
+	-e '/^pkgdatadir/c\pkgdatadir = os.getenv("APPDIR", "/usr") + "/share/system-monitoring-center"' \
+	-e '/^localedir/c\localedir = os.getenv("APPDIR", "/usr") + "/share/locale"' \
+	./AppDir/bin/system-monitoring-center
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
