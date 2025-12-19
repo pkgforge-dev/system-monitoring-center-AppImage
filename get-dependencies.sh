55362682c6bc32ba9130c4048a966ff4127de045
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-12-19 15:27:24 -0400

    get app

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,13 +6,13 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm amdgpu_top
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+make-aur-package system-monitoring-center
 
 # If the application needs to be manually built that has to be done down here
