a24e50dfa2e409cef6be9d7b938233d322b6c6af
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-03-01 23:30:13 -0400

    package now depends on `python-sv-ttk`

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -13,6 +13,7 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
+make-aur-package python-sv-ttk
 make-aur-package system-monitoring-center
 
 # If the application needs to be manually built that has to be done down here
